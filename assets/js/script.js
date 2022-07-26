838fddf69a47fa8dc7067bbce9e3086406a9476d
Jessica Fransson <jessicafransson@outlook.com>
2022-07-26 06:40:07 +0000

    results and styling added to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,43 +27,39 @@ playerOptions.forEach(option => {
             })
 
         /** How to decide winner */
-        const winner = (player,computer) => {
+        const winner = (player, computer) => {
             const result = document.querySelector('.result');
             const playerScoreBoard = document.querySelector('.p-count');
             const computerScoreBoard = document.querySelector('.c-count');
-            player = player.toLowerCase ();
-            if(player === computer) {
+            player = player.toLowerCase();
+            if (player === computer) {
                 result.textContent = 'Tie'
-            }
-            else if(player == 'rock'){
-                if(computer == 'paper') {
+            } else if (player == 'rock') {
+                if (computer == 'paper') {
                     result.textContent = 'Computer Won';
                     computerScore++;
                     computerScoreBoard.textContent = computerScore;
-                }else{
+                } else {
                     result.textContent = 'Player Won'
                     playerScore++;
                     playerScoreBoard.textContent = playerScore;
                 }
-            }
-
-            else if(player == 'scissors') {
-                if(computer == 'rock') {
+            } else if (player == 'scissors') {
+                if (computer == 'rock') {
                     result.textContent = 'Computer Won';
                     computerScore++;
                     computerScoreBoard.textContent = computerScore;
-                }else{
+                } else {
                     result.textContent = 'Player Won';
                     computerScore++;
                     computerScoreBoard.textContent = computerScore;
                 }
-            }
-            else if(player == 'paper') {
-                if(computer == 'scissors') {
+            } else if (player == 'paper') {
+                if (computer == 'scissors') {
                     result.textContent = 'Computer Won';
                     computerScore++;
                     computerScoreBoard.textContent = computerScore;
-                }else{
+                } else {
                     result.textContent = 'Player Won';
                     playerScore++;
                     playerScoreBoard.textContent = playerScore;
@@ -78,4 +74,38 @@ playerOptions.forEach(option => {
 
         if (moves == 10) {
             gameOver(playerOptions, movesLeft);
+        }
+
+        /** Functions to display when game over */
+        const gameOver = (playerOptions,movesLeft) => {
+            const chooseMove = document.querySelector('.result');
+            const reloadBtn = document.querySelector('.reload');
+
+            playerOptions.forEach(option => {
+                option.style.display = 'none';
+            })
+
+            chooseMove.innerText = 'Game over!'
+            movesLeft.style.display = 'none';
+
+            if(playerScore > computerScore) {
+                result.style.fontSize = '2rem';
+                result.innerText = 'You won'
+                result.style.color = '#dca99a'
+            }
+            else if(playerScore < computerScore) {
+                result.style.fontSize = '2rem';
+                result.innerText = 'You lost!';
+                result.style.color = '	#fe2e2e'
+            }
+            else{
+                result.style.fontSize = '2rem';
+                result.innerText = 'Its a tie!';
+                result.style.color = '#5b3849'
+            }
+            reloadBtn.innerText = 'Play Again!';
+            reloadBtn.style.display = 'flex';
+            reloadBtn.addEventListener('click', () => {
+                window.location.reload();
+            })
         }
