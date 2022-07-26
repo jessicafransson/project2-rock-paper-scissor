5ab1ae2a3a1fb17d9df554cce2851947846a22b2
Jessica Fransson <jessicafransson@outlook.com>
2022-07-26 06:03:39 +0000

    added functions to the game and how to start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
-/** The logic of the game */
+/** The logic of the game inside here */
 const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
 }
+
+/** Functions to the game inside here */
+const playGame = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorBtn = document.querySelector('.scissor');
+    const playerOptions = [rockBtn, paperBtn, scissorBtn];
+    const computerOptions = ['rock', 'paper', 'scissors'];
+}
+
+/** Functions to start the game */
+playerOptions.forEach(option => {
+    option.addEventListener('click', function() {
+        const movesLeft = document.querySelector('.movesleft');
+        moves++;
+        movesLeft.innerText = `Moves Left:  ${10-moves}`);
+
+        const choiceNumber = Math.floor(Math.random() *3);
+        const computerChoice = computerOptions[choiseNumber];
+        
+    })
+})
