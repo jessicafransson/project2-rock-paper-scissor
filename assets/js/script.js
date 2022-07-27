3ee864535c0ab25d9aa3e72989042ca0597b8189
Jessica Fransson <jessicafransson@outlook.com>
2022-07-27 15:15:44 +0000

    writing whos winning in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,11 +18,43 @@ const SELECTIONS = [
     }
 ]
 
+/**Specify how to select the different buttons*/
+
+selectionButtons.forEach(selectionButtons => {
+    selectionButton.addEventListener('click', e => {
+       const selectionName = selectionButton.dataSet.selection
+       const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selectionName)
+    })
+})
+
+/** Random selection form computer to game */
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    console.log(computerSelection)
+}
+
 /** Deciding winner */
 
 function makeSelection(selection) {
     const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
-    console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
 }
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+/**  */
