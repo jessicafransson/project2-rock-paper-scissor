4b899876096a0589df8d93bfed1a4c900b8be42b
Jessica Fransson <jessicafransson@outlook.com>
2022-07-28 12:24:08 +0000

    ran code thru validator, pasted with semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,62 @@
 /**DOM manipluators */
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
 
 /** Display the computer and user choices */
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice 
-    generateComputerChoice()
-    getResult()
-}))
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice; 
+    generateComputerChoice();
+    getResult();
+}));
 
-/**How to get the computer choice */
+/**How to get the computer choice 
+ * Code from Code with Ania Kubów */
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if (randomNumber === 1) {
-        computerChoice = 'rock'
+        computerChoice = 'Rock';
     }
     if (randomNumber === 2) {
-        computerChoice = 'scissors'
+        computerChoice = 'Scissors';
     }
     if (randomNumber === 3) {
-        computerChoice = 'paper'
+        computerChoice = 'Paper';
     }
-    computerChoiceDisplay.innerHTML = computerChoice
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-/** How to get the results */
+/** How to get the results, code inspiration from Code with Ania Kubów */
 
 function getResult() {
     if (computerChoice === userChoice) {
-        result = 'its a draw!'
+        result = 'Its a draw!';
     }
-    if (computerChoice === 'rock' && userChoice === "paper") {
-        result = 'you win!'
+    if (computerChoice === 'Rock' && userChoice === "Paper") {
+        result = 'You win!';
     }
-    if (computerChoice === 'rock' && userChoice === "scissors") {
-        result = 'you lost!'
+    if (computerChoice === 'Rock' && userChoice === "Scissors") {
+        result = 'You lost!';
     }
-    if (computerChoice === 'paper' && userChoice === "scissors") {
-        result = 'you win!'
+    if (computerChoice === 'Paper' && userChoice === "Scissors") {
+        result = 'You win!';
     }
-    if (computerChoice === 'paper' && userChoice === "rock") {
-        result = 'you lost!'
+    if (computerChoice === 'Paper' && userChoice === "Rock") {
+        result = 'You lost!';
     }
-    if (computerChoice === 'scissors' && userChoice === "rock") {
-        result = 'you win!'
+    if (computerChoice === 'Scissors' && userChoice === "Rock") {
+        result = 'You win!';
     }
-    if (computerChoice === 'scissors' && userChoice === "paper") {
-        result = 'you lost!'
+    if (computerChoice === 'Scissors' && userChoice === "Paper") {
+        result = 'You lost!';
     }
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
 }
