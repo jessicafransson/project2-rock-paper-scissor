1c90a310702fdb17de8efb7625ddadbd752b9dd7
Jessica Fransson <jessicafransson@outlook.com>
2022-07-28 15:08:42 +0000

    changed the result text and functionallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,55 @@
-/**DOM manipluators */
+/** DOM manipluators */
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
-let userChoice;
-let computerChoice;
-let result;
+
+const ROCK = 'Rock';
+const PAPER = 'Paper';
+const SCISSORS = 'Scissors';
+const options = [ROCK, PAPER, SCISSORS];
 
 /** Display the computer and user choices */
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id;
-    userChoiceDisplay.innerHTML = userChoice; 
-    generateComputerChoice();
-    getResult();
-}));
+possibleChoices.forEach(possibleChoice => {
+  possibleChoice.addEventListener('click', (e) => {
+    const userChoice = e.currentTarget.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    const computerChoice = generateComputerChoice();
+    getResult(userChoice, computerChoice);
+});
+});
 
-/**How to get the computer choice 
+/** How to get the computer choice
  * Code from Code with Ania Kubów */
-
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    if (randomNumber === 1) {
-        computerChoice = 'Rock';
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'Scissors';
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'Paper';
-    }
+    const computerChoice = options[randomNumber-1];
     computerChoiceDisplay.innerHTML = computerChoice;
+    return computerChoice;
 }
 
 /** How to get the results, code inspiration from Code with Ania Kubów */
+function getResult(userChoice, computerChoice) {
+    let result = '';
+    const WIN_TEXT = 'You win!';
+    const LOSE_TEXT = 'You lost!';
 
-function getResult() {
     if (computerChoice === userChoice) {
         result = 'Its a draw!';
+    } else if (computerChoice === ROCK && userChoice === PAPER) {
+        result = WIN_TEXT;
+    } else if (computerChoice === ROCK && userChoice === SCISSORS) {
+        result = LOSE_TEXT;
+    } else if (computerChoice === PAPER && userChoice === SCISSORS) {
+        result = WIN_TEXT;
+    } else if (computerChoice === PAPER && userChoice === ROCK) {
+        result = LOSE_TEXT;
+    } else if (computerChoice === SCISSORS && userChoice === ROCK) {
+        result = WIN_TEXT;
+    } else if (computerChoice === SCISSORS && userChoice === PAPER) {
+        result = LOSE_TEXT;
     }
-    if (computerChoice === 'Rock' && userChoice === "Paper") {
-        result = 'You win!';
-    }
-    if (computerChoice === 'Rock' && userChoice === "Scissors") {
-        result = 'You lost!';
-    }
-    if (computerChoice === 'Paper' && userChoice === "Scissors") {
-        result = 'You win!';
-    }
-    if (computerChoice === 'Paper' && userChoice === "Rock") {
-        result = 'You lost!';
-    }
-    if (computerChoice === 'Scissors' && userChoice === "Rock") {
-        result = 'You win!';
-    }
-    if (computerChoice === 'Scissors' && userChoice === "Paper") {
-        result = 'You lost!';
-    }
+
     resultDisplay.innerHTML = result;
 }
